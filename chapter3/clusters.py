fa44caf50ddadf945b40b8bb5bea0a625638ed68
Ronald Suwandi <ronald@ronaldsuwandi.com>
2016-06-22 21:23:31 +0800

    chapter 3

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from PIL import Image, ImageDraw
 import random
 
 
@@ -91,22 +92,84 @@ def hcluster(rows, distance=pearson):
 
 def print_cluster(cluster, labels=None, n=0):
     for i in range(n):
-        print(' ', end='')
+        print(' ')
     if cluster.id < 0:
         # Negative id == branch
         print('-')
     else:
         # Positive id == endpoint
-        if labels == None:
+        if labels is None:
             print(cluster.id)
         else:
             print(labels[cluster.id])
 
     # Print right and left branches
-    if cluster.left != None:
+    if cluster.left is not None:
         print_cluster(cluster.left, labels=labels, n=n + 1)
-    if cluster.right != None:
+    if cluster.right is not None:
         print_cluster(cluster.right, labels=labels, n=n + 1)
+
+
+def get_height(clust):
+    # If this is an endpoint, then height is just 1
+    if clust.left is None and clust.right is None:
+        return 1
+
+    # Otherwise the height is the same of the heights of each branch
+    return get_height(clust.left) + get_height(clust.right)
+
+
+def get_depth(clust):
+    # The distance of an endpoint is 0
+    if clust.left is None and clust.right is None:
+        return 0
+
+    # The distance of a branch is the grater of its two sides plus its own distance
+    return max(get_depth(clust.left), get_depth(clust.right)) + clust.distance
+
+
+def draw_node(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = get_height(clust.left) * 20
+        h2 = get_height(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+
+        line_len = clust.distance * scaling
+
+        # Vertical line from this cluster to children
+        draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill=(0, 255, 0))
+
+        # Horizontal line to the left item
+        draw.line((x, top + h1 / 2, x + line_len, top + h1 / 2), fill=(255, 0, 0))
+
+        # Horizontal line to the right item
+        draw.line((x, bottom - h2 / 2, x + line_len, bottom - h2 / 2), fill=(255, 0, 0))
+
+        # Draw left and right nodes
+        draw_node(draw, clust.left, x + line_len, top + h1 / 2, scaling, labels)
+        draw_node(draw, clust.right, x + line_len, bottom - h2 / 2, scaling, labels)
+    else:
+        # Draw endpoint if this is an item label
+        draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
+
+def draw_dendogram(clust, labels, jpeg='clusters.jpg'):
+    h = get_height(clust) * 20
+    w = 1200
+    depth = get_depth(clust)
+
+    # scale distance based on width
+    scaling = float(w - 150) / depth
+
+    # Create new image with white bg
+    img = Image.new('RGB', (w, h), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.line((0, h / 2, 10, h / 2), fill=(255, 0, 0))
+
+    # Draw the first node
+    draw_node(draw, clust, 10, (h / 2), scaling, labels)
+    img.save(jpeg, 'JPEG')
 
 
 def rotatematrix(data):
@@ -161,3 +224,72 @@ def kclusters(rows, distance=pearson, k=4):
                 clusters[i] = avgs
 
     return bestmatches
+
+
+def scaledown(data, distance=pearson, rate=0.01):
+    n = len(data)
+
+    # Real distance between every pair of items
+    real_dist = [[distance(data[i], data[j]) for j in range(n)]
+                 for i in range(0, n)]
+
+    outersum = 0.0
+
+    # Randomly initialize starting points
+    loc = [[random.random(), random.random()] for i in range(n)]
+    fake_dist = [[0.0 for j in range(n)] for i in range(n)]
+
+    lasterror = None
+    for m in range(0, 1000):
+        # Find projected distance
+        for i in range(n):
+            for j in range(n):
+                fake_dist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2)
+                                            for x in range(len(loc[i]))]))
+
+        # Move points
+        grad = [[0.0, 0.0] for i in range(n)]
+
+        totalerror = 0
+        for k in range(n):
+            for j in range(n):
+                if j == k:
+                    continue
+
+                # Error is percent diff between distances
+                errorterm = (fake_dist[j][k] - real_dist[j][k]) / real_dist[j][k]
+
+                # Each points needs to be moved away from or towards the other point in proportion to how much error
+                # it has
+                grad[k][0] += ((loc[k][0] - loc[j][0]) / fake_dist[j][k]) * errorterm
+                grad[k][1] += ((loc[k][1] - loc[j][1]) / fake_dist[j][k]) * errorterm
+
+                # Keep track of total error
+                totalerror += abs(errorterm)
+
+        print(totalerror)
+
+        # If answer got worse by moving the points, we are done
+        if lasterror and lasterror < totalerror:
+            break
+
+        lasterror = totalerror
+
+        # Move each points by the learning rate times the gradient
+        for k in range(n):
+            loc[k][0] -= rate * grad[k][0]
+            loc[k][1] -= rate * grad[k][1]
+
+    return loc
+
+
+def draw2d(data, labels, jpeg='mds2d.jpg'):
+    img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+
+    for i in range(len(data)):
+        x = (data[i][0] + 0.5) * 1000
+        y = (data[i][1] + 0.5) * 1000
+        draw.text((x, y), labels[i], (0, 0, 0))
+
+    img.save(jpeg, 'JPEG')
