b7d20df2e6347c84a7d9a18563122fe44b789700
Ronald Suwandi <ronald@ronaldsuwandi.com>
2016-09-14 21:10:11 +0800

    naive bayes

--- chapter6/docclass.py
+++ chapter6/docclass.py
@@ -1,6 +1,7 @@
 import re
 import math
 
+
 def get_words(doc):
     splitter = re.compile('\\W*')
     words = [s.lower() for s in splitter.split(doc)
@@ -8,6 +9,7 @@ def get_words(doc):
 
     # Return the unique set of words only
     return dict([(w, 1) for w in words])
+
 
 def sample_train(classifier):
     classifier.train('Nobody owns the water.', 'good')
@@ -76,19 +78,59 @@ class Classifier:
         prob = probability_fn(feature, category)
 
         # Count all number of times this feature has appeared in all categories
-        total_appeared = sum([self.feature_count(feature, category) for category in self.categories()])
+        total_appeared = sum(
+            [self.feature_count(feature, category) for category in self.categories()])
 
         # Calculate the weighted average
-        return ((weight * assumed_probability) + (total_appeared * prob)) / (weight+total_appeared)
+        return ((weight * assumed_probability) + (total_appeared * prob)) / (
+        weight + total_appeared)
 
 
 class NaiveBayes(Classifier):
+    def __init__(self, get_features):
+        Classifier.__init__(self, get_features)
+        self.thresholds = {}
+
     def document_probability(self, item, category):
         features = self.get_features(item)
 
         # Multiply the probabilities of all features together
         p = 1
         for feature in features:
-            p *= self.weighted_probability(feature, category,  self.feature_probability)
+            p *= self.weighted_probability(feature, category, self.feature_probability)
 
         return p
+
+    def probability(self, item, category):
+        p_category = self.category_count(category) / self.total_count()
+        p_document = self.document_probability(item, category)
+        return p_document * p_category
+
+    def set_threshold(self, category, threshold):
+        self.thresholds[category] = threshold
+
+    def get_threshold(self, category):
+        if category not in self.thresholds:
+            return 1.0
+        return self.thresholds[category]
+
+    def classify(self, item, default=None):
+        probs = {}
+
+        # Find category with highest probs
+        max_prob = 0.0
+        for category in self.categories():
+            probs[category] = self.probability(item, category)
+            if probs[category] > max_prob:
+                max_prob = probs[category]
+                best_category = category
+
+        # Make sure probability > threshold * next best
+        for category in probs:
+            if category == best_category:
+                continue
+
+            if probs[category] * self.get_threshold(best_category) > probs[best_category]:
+                return default
+
+        return best_category
